8bbb475f8420087e39b819b19c14be6d057af046
Martina <tinailievska82@gmail.com>
2025-05-20 20:10:52 +0200

    fix

--- controler/authControler.js
+++ controler/authControler.js
@@ -3,16 +3,40 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { promisify } = require("util");
 
-exports.signup = async (req, res) => {
+exports.signUp = async (req, res) => {
   try {
-    const newUser = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.status(201).json({
-      status: "Success",
 
+   
+    const newUser = await User.create({
+      ime: req.body.ime,
+      age: req.body.age,
+      password: req.body.password,
+      email: req.body.email,
+    });
+
+    const token = jwt.sign(
+      {
+        ime: newUser.ime,
+        id: newUser._id,
+        email: newUser.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_EXPIRES,
+      }
+    );
+
+    res.cookie("jwt", token, {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+      ),
+      secure: false,
+      httpOnly: true,
+    });
+
+    res.status(201).json({
+      status: "Created",
+      token,
       data: {
         user: newUser,
       },
@@ -25,81 +49,92 @@ exports.signup = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+exports.logIn = async (req, res) => {
 
-    if (!email || !password) {
-      return res.status(400).send("Please provide email or password!");
+    try{
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(401).send("Please enter email or password");
+          }
+    
+         const user = await User.findOne({ email });
+        if (!user) {
+          return res.status(401).send("Invalid email or password");
+        }
+    
+        unesenpass = crypto.createHash('sha256').update(password).digest('hex');
+    
+        const validPassword = bcrypt.compareSync(password, user.password);
+    
+        if (!validPassword) {
+            return res.status(401).send("Invalid email or password");
+          }
+     
+          const token = jwt.sign(
+            {
+                id: user._id,
+                ime: user.ime,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: process.env.JWT_EXPIRES,
+            }
+          )
+    
+          res.cookie("jwt", token, {
+            expires: new Date(
+              Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+            ),
+            secure: false,
+            httpOnly: true,
+          });
+    
+    
+          res.status(201).json({
+            status: "success",
+            token: token,
+            user,
+          });
     }
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).send("Invalid email or password");
+    catch(err){
+        res.status(500).json({
+            status: "fail",
+            err: err.message,
+          });
     }
+      
 
-    const isPasswordValid = bcrypt.compareSync(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).send("Invalid email or password");
-    }
+}
 
-    const token = jwt.sign(
-      { id: user._id, name: user.name, email: user.email, role: user.role },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: process.env.JWT_EXPIRES,
-      }
-    );
-
-    res.cookie("jwt", token, {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 10000
-      ),
-      secure: false,
-      httpOnly: true,
-    });
-
-    res.status(201).json({
-      status: "success",
-      token,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      err: err.message,
-    });
-  }
-};
 
 exports.protect = async (req, res, next) => {
-  try {
-    let token;
-
-    if (req.headers.authorization) {
-      token = req.headers.authorization.split(" ")[1];
-    }
-    if (req.cookies.jwt) {
-      token = req.cookies.jwt;
-    }
-
-    if (!token) {
-      return res.status(500).send("You are not logged in! please log in");
-    }
-
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-    const userTrue = await User.findById(decoded.id);
-    if (!userTrue) {
-      return res.status(401).send("User doesnt longer exist!");
-    }
-
-    req.auth = userTrue;
-
-    next();
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      err: err.message,
-    });
+    try{
+let token 
+if (req.headers.authorization) {
+    token = req.headers.authorization.split(" ")[1];
   }
-};
+  if (!token) {
+    return res.status(500).send("You are not logged in, please log in");
+  }
+
+  const decoded = await util.promisify(jwt.verify)(
+       token,
+       process.env.JWT_SECRET
+  )
+  const userTrue = await User.findById(decoded.id);
+  if (!userTrue) {
+    return res.status(401).send("User does not longer exist!");
+  }
+  req.auth = userTrue;
+
+  next();
+    }
+    catch(err){
+        res.status(500).json({
+            status: "fail",
+            err: err.message,
+          }); 
+    }
+}
